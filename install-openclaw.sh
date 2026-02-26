89cbacf6b12dea1212c96a9d0ce17a235e7f0886
liangzhanle-max <liangzhanle@gmail.com>
2026-02-26 18:11:44 +0800

    Add install script for OpenClaw

---
+++ install-openclaw.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Installing OpenClaw..."
+
+echo "Current Date and Time (UTC): 2026-02-26 10:11:38"
+echo "Current User's Login: liangzhanle-max"
+
+# Add the commands to install OpenClaw below
+
+# Example for installation commands:
+# apt-get update
+# apt-get install openclaw
